b390ece2316c393d411006b59febb1f0725f972e
KundaiClayton <chtkun003@myuct.ac.za>
2020-07-19 22:00:01 +0200

    Updates

---
+++ precache-manifest.0dc3f4487a0b89281998d7e2b5b82744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c8e691a482ef9cf03dc11a84363910",
+    "url": "/RonaStats/index.html"
+  },
+  {
+    "revision": "c7f9a89e6e0d7771b269",
+    "url": "/RonaStats/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "79a5cd5c2a79709ec150",
+    "url": "/RonaStats/static/css/main.4ba215dd.chunk.css"
+  },
+  {
+    "revision": "c7f9a89e6e0d7771b269",
+    "url": "/RonaStats/static/js/2.204c4eef.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/RonaStats/static/js/2.204c4eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a5cd5c2a79709ec150",
+    "url": "/RonaStats/static/js/main.8d670f1b.chunk.js"
+  },
+  {
+    "revision": "7ce08daf06295e472503",
+    "url": "/RonaStats/static/js/runtime-main.e59cfcdf.js"
+  }
+]);
