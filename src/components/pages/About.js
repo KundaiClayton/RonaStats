75afec0771ba08703cc408dea8f99081ccd3eb5b
KundaiClayton <chtkun003@myuct.ac.za>
2020-07-27 15:33:16 +0200

    changes in the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,10 @@ function About() {
         <div className="container">
             <h1>About RonaStats App</h1>
             <p>RonaStats is an app with all the information about corona virus in a central place</p>
+            <hr/>
+             <h2>How the ranking works</h2>
+             <p>The ranking is based on population size.</p>
+             <p>For countries with a bigger population like the USA the rankings are according to states/provinces</p>
         </div>
     )
 }
